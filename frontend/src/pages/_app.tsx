29a15cea3d01af6a3265a90b7c930dd5db7fcef2
OscarPALISSOT <oscar.p25000@gmail.com>
2024-02-21 12:41:24 +0100

    init next frontend homepage

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type {AppProps} from 'next/app'
+import '@/styles/globals.css'
+import Head from "next/head";
+
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <title>Oscar PALISSOT | Portfolio</title>
+            </Head>
+
+            <Component {...pageProps} />
+
+        </>
+    )
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {createDirectus, rest} from "@directus/sdk";
+
+//const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest());
+
+const Home = () => {
+
+    return (
+        <>
+            <h1>home</h1>
+        </>
+    )
+}
+export default Home
