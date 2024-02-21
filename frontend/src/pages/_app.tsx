8721dcf0acd0d26876b044b04716c29d964a4af8
OscarPALISSOT <oscar.p25000@gmail.com>
2024-02-21 19:49:53 +0100

    fix apostrophe title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({Component, pageProps}: AppProps) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Le jardin d'Oscar | Terrasses bois et artisan dans le Haut-Doubs</title>
+                <title>Le jardin d&apos;Oscar | Terrasses bois et artisan dans le Haut-Doubs</title>
             </Head>
 
             <Component {...pageProps} />
