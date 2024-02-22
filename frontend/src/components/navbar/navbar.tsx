cd97d3f34457fa217f0ba6495e99ef2bffff7638
OscarPALISSOT <oscar.p25000@gmail.com>
2024-02-22 12:53:08 +0100

    navbar

---
+++ frontend/src/components/navbar/navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface NavbarProps {
+    links: string[];
+    logo: string;
+}
+
+const Navbar = ({links, logo}: NavbarProps) => {
+
+    return (
+        <>
+            <div className={'text-2xl font-serif bg-primary -mx-24 px-24 h-20 flex flex-row justify-between items-center mb-6'}>
+                <h1>navbar</h1>
+                <div>
+                    <ul className={'flex flex-row'}>
+                        {links.map((link, index) => {
+                            return (
+                                <li key={index} className={'mx-4 cursor-pointer'}>{link}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Navbar
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {createDirectus, rest} from "@directus/sdk";
+import Navbar from "@/components/navbar/navbar";
 
 //const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest());
 
@@ -7,6 +8,11 @@ const Home = () => {
 
     return (
         <>
+            <Navbar
+                links={['home', 'about', 'contact', 'services']}
+                logo={'/logo.png'}
+            />
+
             <h1 className={'font-serif'}>home</h1>
             <p>lorem ipsum</p>
         </>
