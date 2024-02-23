c1843d0c43f7c4c56229f836d59971b4a3d23a1d
OscarPALISSOT <oscar.p25000@gmail.com>
2024-02-23 16:58:22 +0100

    add navbar link effect

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useScroll from "@/hooks/useScrool";
+import Link from "next/link";
 
 interface NavbarProps {
     links: string[];
@@ -20,13 +21,19 @@ const Navbar = ({links, logo}: NavbarProps) => {
                         <ul className={'flex flex-row'}>
                             {links.map((link, index) => {
                                 return (
-                                    <li key={index} className={'mx-4 cursor-pointer'}>{link}</li>
+                                    <li key={index} className={'mx-4 cursor-pointer relative'}>
+                                        <Link href={`/#${link}`}
+                                              className={'before:content-[""] before:absolute before:h-0.5 before:bottom-0.5 before:bg-primary before:transition-transform before:w-full before:left-0 before:ease-out before:duration-300 before:origin-bottom-right before:scale-x-0 hover:before:scale-x-100 hover:before:origin-bottom-left'}>
+                                            {link}
+                                        </Link>
+                                    </li>
                                 )
                             })}
                         </ul>
                     </div>
                 </div>
-                <div className={`h-0.5 -mx-8 md:-mx-24 bg-primary origin-right duration-500 scale-x-0 ease-in ${scrollY > 0 && 'scale-x-100'}`}/>
+                <div
+                    className={`h-0.5 -mx-8 md:-mx-24 bg-primary origin-right duration-500 scale-x-0 ease-in ${scrollY > 0 && 'scale-x-100'}`}/>
             </div>
         </>
     )
