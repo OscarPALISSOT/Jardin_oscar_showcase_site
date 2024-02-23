55e59e9e8d5145bb079ee95d4e0af96b3a6c5d7b
OscarPALISSOT <oscar.p25000@gmail.com>
2024-02-23 15:49:46 +0100

    navbar effects

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import useScroll from "@/hooks/useScrool";
 
 interface NavbarProps {
     links: string[];
@@ -7,19 +8,25 @@ interface NavbarProps {
 
 const Navbar = ({links, logo}: NavbarProps) => {
 
+    const scrollY = useScroll();
+
     return (
         <>
-            <div className={'text-2xl font-bold font-serif -mx-24 px-24 h-20 flex flex-row justify-between items-center mb-6'}>
-                <h1>navbar</h1>
-                <div>
-                    <ul className={'flex flex-row'}>
-                        {links.map((link, index) => {
-                            return (
-                                <li key={index} className={'mx-4 cursor-pointer'}>{link}</li>
-                            )
-                        })}
-                    </ul>
+            <div
+                className={`text-2xl transition duration-300 font-bold bg-background font-serif sticky top-0 -mx-24 px-24 mb-6 flex flex-col justify-between ${scrollY > 0 && 'shadow-[0_6px_12px_0_rgba(0,0,0,0.12)] '}`}>
+                <div className={'flex flex-row justify-between items-center h-20'}>
+                    <h1>navbar</h1>
+                    <div>
+                        <ul className={'flex flex-row'}>
+                            {links.map((link, index) => {
+                                return (
+                                    <li key={index} className={'mx-4 cursor-pointer'}>{link}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                 </div>
+                <div className={`h-0.5 -mx-8 md:-mx-24 bg-primary origin-right duration-500 scale-x-0 ease-in ${scrollY > 0 && 'scale-x-100'}`}/>
             </div>
         </>
     )
