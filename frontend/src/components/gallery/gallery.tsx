b251578b24167ecc93cfd365642a97ba7a4c3aa4
OscarPALISSOT <oscar.p25000@gmail.com>
2024-09-28 22:15:53 +0200

    fixed renovation section responsive ipad

--- frontend/src/components/gallery/gallery.tsx
+++ frontend/src/components/gallery/gallery.tsx
@@ -8,7 +8,7 @@ interface GalleryProps {
 const Gallery = ({galleryBlock}: GalleryProps) => {
     return (
         <>
-            <div className={'w-full mb-4 md:my-4 flex flex-col items-center'}>
+            <div className={'w-full lg:my-4 flex flex-col items-center'}>
                 <h1 className={'text-2xl lg:text-3xl xl:text-5xl font-serif font-extrabold text-fontColor mb-1.5 md:mb-4'}>{galleryBlock.title}</h1>
                 <p className={'text-lg lg:text-xl xl:text-2xl text-fontColor'}>{galleryBlock.text}</p>
             </div>
--- frontend/src/components/renovation.tsx
+++ frontend/src/components/renovation.tsx
@@ -9,34 +9,44 @@ interface RenovationProps {
 
 const Renovation = ({renovationBlock}: RenovationProps) => {
     return (
-        <div className={'w-full my-12 flex flex-col sm:flex-row sm:items-center'}>
-            <div className={'mb-6 sm:me-4 sm:w-1/2'}>
+        <div className={'my-12'}>
+            <div className={'hidden md:block lg:hidden'}>
                 <h1 className={'text-3xl lg:text-4xl font-serif font-extrabold text-fontColor mb-2'}>{renovationBlock.title}</h1>
                 <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text}</p>
-                <div className={'mt-8 w-full flex sm:flex-row gap-6 flex-col'}>
-                    <div className={'flex flex-col gap-2 sm:w-1/2'}>
-                        <Image
-                            className={'h-10 mb-4 w-fit hover:scale-105 duration-300 ease-out transition-all'}
-                            src={process.env.NEXT_PUBLIC_ASSETS_URL + renovationBlock.icon_1 + '?key=logo'} alt={renovationBlock.keyword_1} width={128}
-                            height={128}/>
-                        <p className={'font-serif font-extrabold text-lg lg:text-xl text-fontColor underline decoration-primary underline-offset-2 decoration-2'}>{renovationBlock.keyword_1}</p>
-                        <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text_1}</p>
-                    </div>
-                    <div className={'flex flex-col gap-2 sm:w-1/2'}>
-                        <Image
-                            className={'h-10 mb-4 w-fit hover:scale-105 duration-300 ease-out transition-all'}
-                            src={process.env.NEXT_PUBLIC_ASSETS_URL + renovationBlock.icon_2 + '?key=logo'} alt={renovationBlock.keyword_2} width={128}
-                            height={128}/>
-                        <p className={'font-serif font-extrabold text-lg lg:text-xl text-fontColor underline decoration-primary underline-offset-2 decoration-2'}>{renovationBlock.keyword_2}</p>
-                        <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text_2}</p>
+            </div>
+            <div className={'w-full flex flex-col sm:flex-row sm:items-center'}>
+                <div className={'mb-6 sm:me-4 sm:w-1/2'}>
+                <span className={'block md:hidden lg:block'}>
+                    <h1 className={'text-3xl lg:text-4xl font-serif font-extrabold text-fontColor mb-2'}>{renovationBlock.title}</h1>
+                    <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text}</p>
+                </span>
+                    <div className={'mt-8 w-full flex sm:flex-row gap-6 flex-col'}>
+                        <div className={'flex flex-col gap-2 sm:w-1/2'}>
+                            <Image
+                                className={'h-10 mb-4 w-fit hover:scale-105 duration-300 ease-out transition-all'}
+                                src={process.env.NEXT_PUBLIC_ASSETS_URL + renovationBlock.icon_1 + '?key=logo'}
+                                alt={renovationBlock.keyword_1} width={128}
+                                height={128}/>
+                            <p className={'font-serif font-extrabold text-lg lg:text-xl text-fontColor underline decoration-primary underline-offset-2 decoration-2'}>{renovationBlock.keyword_1}</p>
+                            <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text_1}</p>
+                        </div>
+                        <div className={'flex flex-col gap-2 sm:w-1/2'}>
+                            <Image
+                                className={'h-10 mb-4 w-fit hover:scale-105 duration-300 ease-out transition-all'}
+                                src={process.env.NEXT_PUBLIC_ASSETS_URL + renovationBlock.icon_2 + '?key=logo'}
+                                alt={renovationBlock.keyword_2} width={128}
+                                height={128}/>
+                            <p className={'font-serif font-extrabold text-lg lg:text-xl text-fontColor underline decoration-primary underline-offset-2 decoration-2'}>{renovationBlock.keyword_2}</p>
+                            <p className={'text-lg lg:text-xl text-fontColor'}>{renovationBlock.text_2}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={'sm:w-1/2'}>
-                <Picture
-                    image={renovationBlock.image}
-                    transformationPreset={'squared-thumb'}
-                />
+                <div className={'sm:w-1/2 md:my-3 lg:m-0'}>
+                    <Picture
+                        image={renovationBlock.image}
+                        transformationPreset={'squared-thumb'}
+                    />
+                </div>
             </div>
         </div>
     )
