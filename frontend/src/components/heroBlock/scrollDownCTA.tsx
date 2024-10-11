e7864cf13917a022bfc7b6e7b0bea8c8e3a04723
OscarPALISSOT <oscar.p25000@gmail.com>
2024-10-11 20:52:05 +0200

    CTA scroll down mouse effect

--- frontend/src/components/heroBlock/scrollDownCTA.tsx
+++ frontend/src/components/heroBlock/scrollDownCTA.tsx
@@ -1,5 +1,7 @@
 import useScroll from "@/hooks/useScrool";
 import navTo from "@/modules/navTo";
+import useMousePosition from "@/hooks/useMousePosition";
+import {useEffect, useState} from "react";
 
 interface ScrollDownCTAProps {
     ctaScroll: string;
@@ -10,6 +12,17 @@ const ScrollDownCTA = ({ctaScroll, nextSection}: ScrollDownCTAProps) => {
 
     const scrollY = useScroll();
     const delays = [0, -0.2, -0.4];
+    const xMouse = useMousePosition().x;
+    const [isHovered, setIsHovered] = useState(false);
+    const [xMove, setXMove] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setXMove((prev) => prev + (xMouse - prev) / 15);
+        }, 8);
+
+        return () => clearInterval(interval);
+    }, [xMouse]);
 
     return (
         <>
@@ -18,6 +31,8 @@ const ScrollDownCTA = ({ctaScroll, nextSection}: ScrollDownCTAProps) => {
                     navTo(e, nextSection)
                 }}
                 className={`hidden hover:cursor-pointer lg:flex justify-center items-center flex-col absolute bottom-0 left-0 right-0 md:-mx-24 2xl:-mx-64 -mx-8 opacity-0 transition duration-300 ${scrollY < 5 && 'opacity-100'}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
             >
                 <p className={' mb-8 text-lg lg:text-xl xl:text-2xl text-fontColor'}>{ctaScroll}</p>
                 <div className={'mb-16'}>
@@ -29,7 +44,10 @@ const ScrollDownCTA = ({ctaScroll, nextSection}: ScrollDownCTAProps) => {
                         ></span>
                     ))}
                 </div>
-                <div className={'translate-y-1/2 z-0 absolute rounded-full w-80 h-80 bg-radial-gradient from-neutral-600 to-65%'}/>
+                <div
+                    className={'z-0 absolute rounded-full w-80 h-80 bg-radial-gradient from-neutral-600 to-65% translate-y-1/2 -translate-x-1/2 '}
+                    style={isHovered ? {left: xMove} : {left: '50%'}}
+                />
             </div>
 
         </>
