da89d5a6b5e9b9515d5d3da6f1fe3e137c07f265
OscarPALISSOT <oscar.p25000@gmail.com>
2024-10-16 01:02:04 +0200

    carrousel touch

--- frontend/src/components/carrousel/carrousel.tsx
+++ frontend/src/components/carrousel/carrousel.tsx
@@ -1,4 +1,3 @@
-import useWidth from "@/hooks/useWidth";
 import {useEffect, useState} from "react";
 import CarrouselItem from "@/components/carrousel/carrouselItem";
 import CarrouselBtn from "@/components/carrousel/carrouselBtn";
@@ -11,6 +10,9 @@ type CarrouselProps = {
 const Carrousel = ({images}: CarrouselProps) => {
 
     const [currentItem, setCurrentItem] = useState(0);
+    const [startTouchX, setStartTouchX] = useState(0);
+    const [isTouching, setIsTouching] = useState(false);
+    const swipeThreshold = document.body.clientWidth / 4;
 
     const goToItem = (index: number) => {
         if (index < 0) {
@@ -26,8 +28,70 @@ const Carrousel = ({images}: CarrouselProps) => {
         document.getElementById('carrousel__container')!.style.transform = `translate3d(${currentItem * -100 / images.length}%, 0, 0)`
     }, [currentItem]);
 
+    useEffect(() => {
+        if (isTouching) {
+            document.getElementById('carrousel__container')!.style.transition = 'none';
+        } else {
+            document.getElementById('carrousel__container')!.style.transition = 'transform 500ms ease-in-out';
+        }
+    }, [isTouching]);
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        setIsTouching(true);
+        setStartTouchX(e.touches[0].clientX);
+    }
+
+    const handleTouchMove = (e: React.TouchEvent) => {
+        const currentTouchX = e.touches[0].clientX;
+        const touchDifference = startTouchX - currentTouchX;
+
+        if (touchDifference > swipeThreshold || touchDifference < -swipeThreshold) {
+            return;
+        }
+
+        if (currentItem === 0 && touchDifference < 0) {
+            return;
+        } else if (currentItem === images.length - 1 && touchDifference > 0) {
+            return;
+        }
+
+        const currentTranslateX = -currentItem * document.getElementById('carrousel__container')!.clientWidth / images.length;
+
+        document.getElementById('carrousel__container')!.style.transform = `translateX(${currentTranslateX - touchDifference}px)`;
+    }
+
+    const handleTouchEnd = (e: React.TouchEvent) => {
+        const endTouchX = e.changedTouches[0].clientX;
+        const touchDifference = startTouchX - endTouchX;
+
+        if (currentItem === 0 && touchDifference < 0) {
+            return;
+        } else if (currentItem === images.length - 1 && touchDifference > 0) {
+            return;
+        }
+
+        if (touchDifference > swipeThreshold) {
+            setIsTouching(false);
+            goToItem(currentItem + 1);
+        } else if (touchDifference < -swipeThreshold) {
+            setIsTouching(false);
+            goToItem(currentItem - 1);
+        } else {
+            setIsTouching(false);
+            document.getElementById('carrousel__container')!.style.transition = 'transform 500ms ease-in-out';
+            document.getElementById('carrousel__container')!.style.transform = `translate3d(${currentItem * -100 / images.length}%, 0, 0)`
+        }
+
+    }
+
+
     return (
-        <div className={'relative h-full overflow-hidden'}>
+        <div
+            className={'relative h-full overflow-hidden'}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        >
             <div
                 id={'carrousel__container'}
                 className={'flex flex-row items-center h-full transition-transform duration-500 ease-in-out'}
