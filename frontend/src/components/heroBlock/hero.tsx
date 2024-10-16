110f32cfe66dae33b20b064e6f8171b8da3da136
OscarPALISSOT <oscar.p25000@gmail.com>
2024-10-16 18:39:43 +0200

    fix margin bottom hero

--- frontend/src/components/heroBlock/hero.tsx
+++ frontend/src/components/heroBlock/hero.tsx
@@ -9,7 +9,7 @@ interface SectionProps {
 const Hero = ({heroBlock, nextSection}: SectionProps) => {
     return (
         <div className={'relative flex-grow flex items-center'}>
-            <div className={'w-full mb-32 flex flex-col sm:flex-row sm:items-center'}>
+            <div className={'w-full lg:mb-32 flex flex-col sm:flex-row sm:items-center'}>
                 <div className={'mb-6 sm:me-4 sm:w-1/2'}>
                     <h1 className={'text-2xl lg:text-3xl xl:text-5xl font-serif font-extrabold text-fontColor mb-4'}>{heroBlock.title}</h1>
                     <p className={'text-lg lg:text-xl xl:text-2xl text-fontColor'}>{heroBlock.subtitle}</p>
