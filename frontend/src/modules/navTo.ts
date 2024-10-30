ed695d63d6fc9c72aa5bfd2518c0622205a2bebd
OscarPALISSOT <oscar.p25000@gmail.com>
2024-10-31 00:31:49 +0100

    navTo update

--- frontend/src/modules/navTo.ts
+++ frontend/src/modules/navTo.ts
@@ -2,13 +2,24 @@ import React from "react";
 
 const navTo = (e: React.MouseEvent<HTMLElement>, link: string) => {
     e.preventDefault();
-    //let url = new URL((e.target as HTMLAnchorElement).href);
-    const section = document.getElementById(link);
-    if (section) {
-        const yOffset = -82;
-        const y = section.getBoundingClientRect().top + window.scrollY + yOffset;
-        window.scrollTo({ top: y, behavior: 'smooth' });
+    const moveTo = (link: string) => {
+        const section = document.getElementById(link);
+        if (section) {
+            const yOffset = -82;
+            const y = section.getBoundingClientRect().top + window.scrollY + yOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
     }
+
+    if (window.location.pathname !== '/') {
+        window.location.assign('/');
+        window.addEventListener('load', () => {
+            moveTo(link);
+        })
+    } else {
+        moveTo(link);
+    }
+
 }
 
 export default navTo;
