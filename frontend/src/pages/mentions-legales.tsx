a47ef7df1845f8ba66596e35b8ccdd006130da4e
OscarPALISSOT <oscar.p25000@gmail.com>
2024-10-31 19:16:49 +0100

    mentions légales page

--- frontend/src/pages/mentions-legales.tsx
+++ frontend/src/pages/mentions-legales.tsx
@@ -9,17 +9,13 @@ const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest()
 interface MentionsLegalesProps {
     links: string[];
     logo: string;
-    heroBlock: HeroType;
-    serviceBlock: Service_blockType;
-    renovationBlock: RenovationType;
-    galleryBlock: GalleryType;
-    contactBlock: ContactType;
+    mentions: string;
 }
 
-const MentionsLegales = ({links, logo}: MentionsLegalesProps) => {
+const MentionsLegales = ({links, logo, mentions}: MentionsLegalesProps) => {
     return (
         <>
-            <div className={'bg-background px-8 md:px-24 2xl:px-64 min-h-[calc(100vh)]'}>
+            <div className={'bg-background px-8 md:px-24 2xl:px-64 h-fit snap-start'}>
                 <NavbarMobile
                     links={links}
                     logo={logo}
@@ -28,10 +24,12 @@ const MentionsLegales = ({links, logo}: MentionsLegalesProps) => {
                     links={links}
                     logo={logo}
                 />
-                <div>
-                    <h1>Mentions légales</h1>
-                    <p>...</p>
-                </div>
+
+                <h1 className={'text-2xl lg:text-3xl xl:text-5xl font-serif font-extrabold text-fontColor mb-8 mt-14'}>Mentions légales</h1>
+                <div
+                    className={'mb-10 xl:mb-44 prose prose-h1:text-xl prose-h1:lg:text-2xl prose-h1:xl:text-4xl prose-h1:font-serif prose-h1:font-extrabold prose-h1:text-fontColor prose-h1:mb-3 prose-h1:mt-10 prose-p:text-lg prose-p:lg:text-xl prose-p:text-fontColor prose-p:my-0 prose-a:cursor-pointer prose-a:text-md lg:text-lg prose-a:text-fontColor hover:prose-a:text-primary prose-a:transition-all hover:prose-a:decoration-0 prose-a:duration-150 prose-a:ease-in-out prose-a:underline prose-a:decoration-2 prose-a:decoration-primary'}
+                    dangerouslySetInnerHTML={{ __html: mentions }}
+                />
                 <Footer links={links}/>
             </div>
         </>
@@ -70,15 +68,17 @@ export async function getServerSideProps() {
         })
     ) as unknown as ContactType;
 
+    const mentions = await client.request(
+        readItems('mentions_legales', {
+            fields: ['*', {}],
+        })
+    ) as unknown as MentionsType;
+
     return {
         props: {
-            heroBlock: hero,
-            serviceBlock: service_block,
-            renovationBlock: renovation,
-            galleryBlock: gallery,
-            contactBlock: contact,
             links: [hero.link, service_block.link, renovation.link, gallery.link, contact.link],
             logo: hero.logo,
+            mentions: mentions.mentions,
         },
     };
 }
