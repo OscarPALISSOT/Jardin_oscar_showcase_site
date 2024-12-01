2fa82a37dddd0fb831601d58a25725687f2c6b84
OscarPALISSOT <oscar.p25000@gmail.com>
2024-12-01 17:45:52 +0100

    update sitemap and robot

--- frontend/src/pages/sitemap.xml.tsx
+++ frontend/src/pages/sitemap.xml.tsx
@@ -1,43 +1,9 @@
-import {createDirectus, readSingleton, rest} from "@directus/sdk";
 import {GetServerSideProps} from "next";
-
-const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest());
 
 const Sitemap: React.FC = () => null
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
-    const hero = await client.request(
-        readSingleton('hero', {
-            fields: ['*', {}],
-        })
-    ) as unknown as HeroType;
-
-    const service_block = await client.request(
-        readSingleton('service_block', {
-            fields: ['link', {}],
-        })
-    ) as unknown as Service_blockType;
-
-    const renovation = await client.request(
-        readSingleton('renovation', {
-            fields: ['link', {}],
-        })
-    ) as unknown as RenovationType;
-
-    const gallery = await client.request(
-        readSingleton('gallery', {
-            fields: ['link', {}],
-        })
-    ) as unknown as GalleryType;
-
-    const contact = await client.request(
-        readSingleton('contact', {
-            fields: ['link', {}],
-        })
-    ) as unknown as ContactType;
-
-    const links = [hero.link, service_block.link, renovation.link, gallery.link, contact.link]
     const homepage = 'https://jardindoscar.fr/'
 
     if (res) {
@@ -48,12 +14,6 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
                 <loc>${homepage}</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
             </url>
-            ${links.map(link => `
-                <url>
-                    <loc>${homepage}#${link}</loc>
-                    <lastmod>${new Date().toISOString()}</lastmod>
-                </url>
-            `).join('')}
         </urlset>`)
         res.end()
     }
