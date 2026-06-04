ee17b92d12a854f47e8a0016680a68e7b96bdb8b
OscarPALISSOT <oscar.p25000@gmail.com>
2026-06-05 00:05:01 +0200

    fix img domain directus

--- frontend/src/components/picture.tsx
+++ frontend/src/components/picture.tsx
@@ -9,7 +9,7 @@ type PictureProps = {
 const Picture = ({image, transformationPreset, style}: PictureProps) => {
     return (
         <div className={`group overflow-hidden rounded-xl flex items-center ${style ?? ''}`}>
-            <Image src={process.env.NEXT_PUBLIC_ASSETS_URL + image + '?key=' + transformationPreset}
+            <Image src={process.env.NEXT_DIRECTUS_INTERNAL_URL + image + '?key=' + transformationPreset}
                    width={1080} height={1080} alt={image}
                    className={'h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out'}/>
         </div>
